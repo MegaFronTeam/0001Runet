2c74c72eb2777f0c69d4e5ba225e39f5b5ccc7c9
Pavel <pakhal1414@gmail.com>
2023-09-08 21:51:58 +0200

    optimize

--- sourse/pug/blocks/headerBlock/main.js
+++ sourse/pug/blocks/headerBlock/main.js
@@ -5,13 +5,13 @@ import * as THREE from "three";
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";  
-import { RGBELoader } from "three/addons/loaders/RGBELoader.js";  
-import { EXRLoader } from 'three/addons/loaders/EXRLoader.js';
+import { RGBMLoader } from "three/addons/loaders/RGBMLoader.js";  
+// import { EXRLoader } from 'three/addons/loaders/EXRLoader.js';
 //Create a Three.JS Scene
 let camera;
 // const monkeyUrl = new URL('/scene1.gltf', import.meta.url);
 // const monkeyUrl = new URL('model/RU-logo-export-anim-3.1.gltf', import.meta.url);
-const monkeyUrl = new URL('model/RU-logo-export-anim-4-raw-2.gltf', import.meta.url); 
+const monkeyUrl = new URL('model/RU-logo-export-anim-4.gltf', import.meta.url); 
 const scene = new THREE.Scene();
 // scene.add(new THREE.GridHelper(20, 20));
 
@@ -47,44 +47,23 @@ const textures = { none: null };
 //Instantiate a loader for the .gltf file
 const dracoLoader = new DRACOLoader();
 
+
  
-dracoLoader.setDecoderPath('https://unpkg.com/browse/three@0.156.1/examples/jsm/libs/draco/')
+dracoLoader.setDecoderPath('https://cdn.jsdelivr.net/npm/three@0.156.1/examples/jsm/libs/draco/')
+// const filename = 'model/ru-hdri-test.hdr';
+// const filename = 'model/ru-hdri-test-3.hdr';
+// const filename = 'model/ru-hdri-test-3.png';
+const filename = 'img/@2x/ru-hdri-test-3-1.png';
+
+let envPath = filename;
+let envMap; 
 
 
 const loader = new GLTFLoader();
 loader.setDRACOLoader(dracoLoader); 
 
-loader.load(
-  // `models/${objToRender}/scene.gltf`,
-  // `statuette/statuette.gltf`,
-  // `ex.gltf`,
-  monkeyUrl.href,
-  // `ex/statuette_rendering.gltf`,
-  // `ex/statuette_rendering.gltf`,
 
-  function (gltf) {
-    //If the file is loaded, add it to the scene
-    object = gltf.scene;  
-    // object = new THREE.Mesh(object, material);
-    object.scale.set(4, 4, 4); 
-    scene.add(object);  
-    mixer = new THREE.AnimationMixer(object);
-    const clips = gltf.animations;
-    clips.forEach(function (clip) {
-      const action = mixer.clipAction(clip);
-      action.play();
-    });
-  },
-  function (xhr) {
-    //While it is loading, log the progress
-    // console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-  },
-  function (error) {
-    //If there is an error, log it
-    console.error(error);
-  }
-);
-
+ 
 
 let w = 1331;
 let h = 3916;
@@ -96,7 +75,7 @@ const renderer = new THREE.WebGLRenderer({
  }); //Alpha: true allows for the transparent background
 renderer.setPixelRatio(window.devicePixelRatio * 12);
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1.2;
+renderer.toneMappingExposure = .094;
 renderer.setClearColor(0x000000, 0); 
 // renderer.setSize(window.innerWidth, window.innerWidth);
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
@@ -107,19 +86,11 @@ renderer.toneMapping = THREE.ACESFilmicToneMapping;
 document.getElementById("container3D").appendChild(renderer.domElement); 
 
 
-// const filename = 'model/ru-hdri-test.hdr';
-const filename = 'model/ru-hdri-test-3.hdr';
-// const filename = 'model/ru-hdri-compressed.png';
-
-let envPath = filename;
-let envMap; 
-
-
 
 var pmremGenerator = new THREE.PMREMGenerator(renderer);
 pmremGenerator.compileEquirectangularShader();
 
-new RGBELoader()
+new RGBMLoader()
   .setDataType(THREE.FloatType)
   .load(envPath, (texture) => {
     envMap = pmremGenerator.fromEquirectangular(texture).texture;
@@ -141,6 +112,56 @@ window.addEventListener("resize", function () {
   renderer.setPixelRatio(window.devicePixelRatio * 12);
   // renderer.setSize(window.innerWidth, window.innerWidth  );
 });
+
+
+
+
+
+
+loader.load( 
+  monkeyUrl.href, 
+  function (gltf) {
+    //If the file is loaded, add it to the scene
+    object = gltf.scene;
+    object.traverse(function (child) {
+      //This allow us to check if the children is an instance of the Mesh constructor
+      if (child instanceof THREE.Mesh) {
+        child.material = new THREE.MeshStandardMaterial({
+          color: 0xEBB86B,
+          roughness: .114,
+          metalness: .56,
+          envMapIntensity: 2,
+          envMap: envMap
+        });
+ 
+        // child.material.map = diffuseMap;
+        // child.material.roughnessMap = roughnessMap;
+        // child.material.needsUpdate = true;
+
+        //Sometimes there are some vertex normals missing in the .obj files, ThreeJs will compute them
+      }
+    });
+    // object = new THREE.Mesh(object, material);
+    object.scale.set(4, 4, 4);
+    scene.add(object);
+    mixer = new THREE.AnimationMixer(object);
+    const clips = gltf.animations;
+    clips.forEach(function (clip) {
+      const action = mixer.clipAction(clip);
+      action.play();
+    });
+  },
+  function (xhr) {
+    //While it is loading, log the progress
+    // console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+  function (error) {
+    //If there is an error, log it
+    console.error(error);
+  }
+);
+
+
  
 function render() {
 
